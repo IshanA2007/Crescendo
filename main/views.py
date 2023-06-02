0bea7c5521ca8f1344e95f2dce1951d6ace412b2
Logan Bradley <loganbradley787@gmail.com>
2023-06-02 15:23:21 -0400

    can actually add files now!!!!!!

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .extras import process
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # from .extras import magenta
 
@@ -10,11 +11,10 @@ def index(request):
     returnfile = None
     if request.method == "POST" and "file" in request.FILES:
         file = request.FILES["file"]
-        file_path = os.path.join("extras/audios", file.name)  # Construct the file path
-
-        with open(file_path, "wb") as destination:
-            for chunk in file.chunks():
-                destination.write(chunk)
+        if file is not None:
+            with open(BASE_DIR + '/main/extras/audios/' + file.name, 'wb') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
     ctx = {
         "file": file,
         "retfile": returnfile,
