73a533e9c1f393bf45d7627d178fd494c45f97ea
Ishan Ajwani <ishan.ajwani.cookie@gmail.com>
2023-06-02 15:56:54 -0400

    views and index changes

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .extras import process
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # from .extras import magenta
@@ -9,18 +10,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 def index(request):
     file = None
     returnfile = None
+    theme = None
     if request.method == "POST" and "file" in request.FILES:
         file = request.FILES["file"]
+        theme = request.POST["theme"]
         if file is not None:
-            with open(BASE_DIR + '/main/extras/audios/' + file.name, 'wb') as destination:
+            with open(
+                BASE_DIR + "/main/extras/audios/" + file.name, "wb"
+            ) as destination:
                 for chunk in file.chunks():
                     destination.write(chunk)
-    ctx = {
-        "file": file,
-        "retfile": returnfile,
-    }
+    ctx = {"file": file, "retfile": None, "theme": theme}
     return render(
         request,
         "index.html",
-        context={"file": file},
+        context=ctx,
     )
